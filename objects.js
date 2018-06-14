1613a7c9e2fffa13ee0844f7af4070fb28febc8a
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 02:11:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,5 @@ var playlist={queen:'sing to me'}
 function updatePlaylist(playlist, artistName, songTitle) {
   // return Object.assign({},{artistName:[songTitle]})
   playlist.artistName=songTitle
+  return playlist
 }
