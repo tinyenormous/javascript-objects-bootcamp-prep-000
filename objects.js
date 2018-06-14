8918c3b7e74353f6881ce1059688d25383f5055f
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 02:19:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  playlist.delete(artistName)
+  delete playlist.artistName
   return playlist
 }
